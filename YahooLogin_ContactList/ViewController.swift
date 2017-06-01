2367e8b10ac2545654909eee9a2423c6e75b57e9
amit.rawat <amit.rawat@int.absas.net>
2017-06-01 14:09:47 +0530

    Initial Commit

---
+++ YahooLogin_ContactList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YahooLogin_ContactList
+//
+//  Created by Abhishek Gupta on 01/06/17.
+//  Copyright © 2017 Abhishek Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
